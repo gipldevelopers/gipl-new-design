4fc0c0ec8e234318eda42d31e3ffbeec267c1983
Panchal Urvisha <urvisha@gohilinfotech.com>
2026-04-24 15:51:34 +0530

    feat: scaffold frontend project architecture and implement modular UI components

---
+++ src/app/technology/page.js
@@ -0,0 +1,15 @@
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import TechnologyPageSections from "@/components/technology/TechnologyPageSections";
+
+export default function TechnologyPage() {
+  return (
+    <>
+      <Navbar />
+      <main className="w-full bg-[#F3F5F9]">
+        <TechnologyPageSections />
+      </main>
+      <Footer />
+    </>
+  );
+}
