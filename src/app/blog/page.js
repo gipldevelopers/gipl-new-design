8bb45ba3259d8bd71e1fd9f317c0da6f7d00d250
Panchal Urvisha <urvisha@gohilinfotech.com>
2026-04-24 15:50:35 +0530

    feat: bootstrap frontend application with modular component architecture and routing

---
+++ src/app/blog/page.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import BlogListingPage from "@/components/blog/BlogListingPage";
+
+export default function BlogPage() {
+  return (
+    <>
+      <Navbar />
+      <BlogListingPage />
+      <Footer />
+    </>
+  );
+}
