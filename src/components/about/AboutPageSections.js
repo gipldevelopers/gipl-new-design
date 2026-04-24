a3b4df58cba30132a10e727d762c9543a47ee0f4
Panchal Urvisha <urvisha@gohilinfotech.com>
2026-04-24 15:52:41 +0530

    Create AboutPageSections.js

---
+++ src/components/about/AboutPageSections.js
@@ -0,0 +1,17 @@
+import AboutHeroSection from "@/components/about/AboutHeroSection";
+import AboutOriginSection from "@/components/about/AboutOriginSection";
+import AboutFounderQuoteSection from "@/components/about/AboutFounderQuoteSection";
+import AboutTeamBeliefsSection from "@/components/about/AboutTeamBeliefsSection";
+
+
+export default function AboutPageSections() {
+  return (
+    <>
+      <AboutHeroSection />
+      <AboutOriginSection />
+      <AboutFounderQuoteSection />
+      <AboutTeamBeliefsSection />
+     
+    </>
+  );
+}
