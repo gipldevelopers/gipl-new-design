7a25760f21b836f837f710b4bc58da7adf08d812
Panchal Urvisha <urvisha@gohilinfotech.com>
2026-04-24 15:54:38 +0530

    Create ServicePageSections.js

---
+++ src/components/service/ServicePageSections.js
@@ -0,0 +1,13 @@
+import ServiceHeroSection from "@/components/service/ServiceHeroSection";
+import ServiceCardsSection from "@/components/service/ServiceCardsSection";
+import ServiceTechnologySection from "@/components/service/ServiceTechnologySection";
+
+export default function ServicePageSections() {
+  return (
+    <>
+      <ServiceHeroSection />
+      <ServiceCardsSection />
+      <ServiceTechnologySection />
+    </>
+  );
+}
