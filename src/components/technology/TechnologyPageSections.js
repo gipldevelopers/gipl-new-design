b883b9c3f9ff1945bc05e16e8372a12243cc3512
Panchal Urvisha <urvisha@gohilinfotech.com>
2026-04-24 15:54:00 +0530

    Create TechnologyPageSections.js

---
+++ src/components/technology/TechnologyPageSections.js
@@ -0,0 +1,13 @@
+import TechnologyHeroSection from "@/components/technology/TechnologyHeroSection";
+import TechnologyCapabilitiesSection from "@/components/technology/TechnologyCapabilitiesSection";
+import TechnologyProcessSection from "@/components/technology/TechnologyProcessSection";
+
+export default function TechnologyPageSections() {
+  return (
+    <>
+      <TechnologyHeroSection />
+      <TechnologyCapabilitiesSection />
+      <TechnologyProcessSection />
+    </>
+  );
+}
