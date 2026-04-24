27c438ede1ed3471d17d25f0fbc53bad64bb9786
Panchal Urvisha <urvisha@gohilinfotech.com>
2026-04-24 15:53:03 +0530

    Create CareerPageSections.js

---
+++ src/components/career/CareerPageSections.js
@@ -0,0 +1,15 @@
+import CareerHeroSection from "@/components/career/CareerHeroSection";
+import CareerBenefitsSection from "@/components/career/CareerBenefitsSection";
+import CareerCultureSection from "@/components/career/CareerCultureSection";
+import CareerOpenRolesSection from "@/components/career/CareerOpenRolesSection";
+
+export default function CareerPageSections() {
+  return (
+    <>
+      <CareerHeroSection />
+      <CareerBenefitsSection />
+      <CareerCultureSection />
+      <CareerOpenRolesSection />
+    </>
+  );
+}
